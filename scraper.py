9c81da1a7b9a3c48a8f89f6eb199a16c99fc7df0
JacobMcMorrow <jacobmcmorrow@rogers.com>
2021-03-16 14:20:28 -0400

    Added directory image scrape and updated organization.

--- scraper.py
+++ scraper.py
@@ -11,8 +11,6 @@ BASE_URL = "http://us.vclart.net"
 ARTISTS_URL = "http://us.vclart.net/vcl/Artists/"
 AUTHORS_URL = ""
 
-# If this gets too bloated, split into separate scrapers.
-
 class Scraper:
     """"""
 
@@ -29,113 +27,123 @@ class Scraper:
 
     def _scrape_artists(self):
         """"""
+        # Make sure a directory for artists exists.
         if not os.path.exists("Artists"):
             os.mkdir("Artists")
 
-        try:
-            self._current_proxies = self.proxies.get_proxies(ARTISTS_URL)
-            response = requests.get(ARTISTS_URL,
-                                    proxies=self._current_proxies,
-                                    headers=self.headers.get_headers())
-            response.encode = "utf-8"
-            response.raise_for_status()
+        # Collect all of the artists.
+        tables = self._collect_tables(ARTISTS_URL)
+        artists_table = tables[4]
+        artists = artists_table.select("a")
 
-            soup = BeautifulSoup(response.text, "html.parser")
-            # Tables aren't named, find a way to double check incase they move
-            # things in the future.
-            artist_table = soup.select("table")[4]
-            artists = artist_table.select("a")
+        # Look into making a generator to ease memory.
+        for artist in artists:
+            self._scrape_artist(artist)
 
-            # Look into making a generator to ease memory.
-            for artist in artists:
-                self._scrape_artist(artist)
-            
-        except Exception as e:
-            print(e)
-            # Write a better error message, come up with a set descriptive set
-            # up and apply that to each file.
-            print("Error connecting")
+        # self._scrape_artist(artists[0])
 
     def _scrape_artist(self, artist):
         """"""
-        self._current_artist = artist.get("href").split("/")[-2]
+        # self._current_artist = artist.get("href").split("/")[-2]
         # self._current_artist = "Selunca"
-        print(self._current_artist)
-        artist_url = BASE_URL + artist.get("href")
+        self._current_artist = "Drakhenliche"
+        # print(self._current_artist)
+        # artist_url = BASE_URL + artist.get("href")
         # artist_url = "http://us.vclart.net/vcl/Artists/Selunca/"
-        
+        artist_url = "http://us.vclart.net/vcl/Artists/Drakhenliche/"
+
+        # Make sure a directory exists for this artist.
         if not os.path.exists(os.path.join("Artists", self._current_artist)):
             os.mkdir(os.path.join("Artists", self._current_artist))
 
-        try:
-            # Multiple proxie requests is excessive. Either grab one at the
-            # start or write a counter to check when to grab the next. Maybe
-            # make it random.
-            self._current_proxies = self.proxies.get_proxies(ARTISTS_URL)
-            response = requests.get(artist_url,
-                                    proxies=self._current_proxies,
-                                    headers=self.headers.get_headers())
-            response.encode = "utf-8"
-            response.raise_for_status()
+        # Get new proxy for this artist.
+        self._current_proxies = self.proxies.get_proxies(ARTISTS_URL)
 
-            soup = BeautifulSoup(response.text, "html.parser")
-            tables = soup.select("table")[1:-1]
+        tables = self._collect_tables(artist_url)
+        media_tables = tables[1:-1]
 
-            for table in tables:
-
-                if table.select("tr")[0].text.find("Directories") >= 0:
-                    print("Directories")
-                elif table.select("tr")[0].text.find("Images") >= 0:
-                    print("Images")
-                    self._collect_images(artist_url, table.select("tr"))
-                elif table.select("tr")[0].text.find("Files") >= 0:
-                    print("Files")
-
-        except Exception as e:
-            print(e)
-            # Write a better error message, come up with a set descriptive set
-            # up and apply that to each file.
-            print("Error connecting")
-
+        for table in media_tables:
+            if table.select("tr")[0].text.find("Directories") >= 0:
+                print("Directories")
+                # self._collect_directories(artist_url, table.select("tr"))
+            elif table.select("tr")[0].text.find("Images") >= 0:
+                print("Images")
+                # directory_path = os.path.join("Artists", self._current_artist)
+                # self._collect_images(artist_url, directory_path,
+                                     table.select("tr"))
+            elif table.select("tr")[0].text.find("Files") >= 0:
+                print("Files")
 
     # Rename these to something more appropriate.
-    def _collect_directories(self):
+    def _collect_directories(self, artist_url, table):
         """"""
-        raise NotImplementedError
+        directory_rows = table[1:]
+
+        for row in directory_rows:
+            directory_info = row.select("a")[0]
+            directory_name = directory_info.getText()
+            directory_url = artist_url + directory_info.get("href")
+            # print(directory_url)
+            self._scrape_directory(directory_name, directory_url)
+
+        # directory_info = directory_rows[0].select("a")[0]
+        # directory_name = directory_info.getText()
+        # directory_url = artist_url + directory_info.get("href")
+        # print(directory_url)
+        # self._scrape_directory(directory_name, directory_url)
+
+    def _scrape_directory(self, directory_name, directory_url):
+        """"""
+        artist_path = os.path.join("Artists", self._current_artist)
+        print(artist_path)
+        directory_path = os.path.join(artist_path, directory_name)
+        print(directory_path)
+
+        # Make sure a directory exists for this directory.
+        if not os.path.exists(directory_path):
+            os.mkdir(directory_path)
+
+        tables = self._collect_tables(directory_url)
+        media_tables = tables[1:-1]
+
+        for table in media_tables:
+            if table.select("tr")[0].text.find("Images") >= 0:
+                print("Images")
+                self._collect_images(directory_url, directory_path,
+                                     table.select("tr"))
+            elif table.select("tr")[0].text.find("Files") >= 0:
+                print("Files")
 
     def _collect_files(self):
         """"""
         raise NotImplementedError
 
-    def _collect_images(self, artist_url, table):
+    def _collect_images(self, url, directory_path, table):
         """"""
         image_rows = table[2:]
+        directory_path = os.path.join(directory_path, "Images")
+        print(url)
+
+        # Make sure a directory exists for these images.
+        if not os.path.exists(directory_path):
+            os.mkdir(directory_path)
 
         for row in image_rows:
             index_by_date = row.select("a")[0]
-            page_url = artist_url + index_by_date.get("href")
-            self._scrape_page(page_url)
+            page_url = url + index_by_date.get("href")
+            self._scrape_page(directory_path, url, page_url)
 
-    def _scrape_page(self, page_url):
+    def _collect_tables(self, url):
         """"""
         try:
-            # Multiple proxie requests is excessive. Either grab one at the
-            # start or write a counter to check when to grab the next. Maybe
-            # make it random.
-            response = requests.get(page_url,
-                                    proxies=self._current_proxies,
+            response = requests.get(url, proxies=self._current_proxies,
                                     headers=self.headers.get_headers())
             response.encode = "utf-8"
             response.raise_for_status()
 
             soup = BeautifulSoup(response.text, "html.parser")
-            image_table = soup.select("table")[1]
 
-            image_rows = image_table.select("img")
-            # self._download_image(image_rows[0].get("alt"))
-
-            for row in image_rows:
-               self._download_image(row.get("alt"))
+            return soup.select("table")
 
         except Exception as e:
             print(e)
@@ -143,23 +151,43 @@ class Scraper:
             # up and apply that to each file.
             print("Error connecting")
 
-    def _download_image(self, image_name):
+    def _scrape_page(self, directory_path, image_url_base, page_url):
         """"""
-        image_url = f"{ARTISTS_URL}{self._current_artist}/{image_name}"
-        # print(image_url)
-        file_name = os.path.join(self._current_artist, image_name)
+        # Collect images on this page.
+        tables = self._collect_tables(page_url)
+        image_table = tables[1]
+        images = image_table.select("img")
+
+        for image in images:
+            image_name = image.get("alt")
+            self._download_image(directory_path, image_name, image_url_base)
+
+    def _download_image(self, directory_path, image_name, image_url_base):
+        """"""
+        # Correct issue in names with %. Keep an eye out for other wrong
+        # symbols. If this becomes too complex, move to a new function.
+        if image_name.find("%") >= 0:
+            edited_name = image_name
+            edited_name.replace("%", "%25")
+            image_url = image_url_base + edited_name
+        else:
+            image_url = image_url_base + image_name
+
+        file_name = os.path.join(directory_path, image_name)
         # print(file_name)
 
         try:
             print(f"Downloading image: {image_name}")
+
+            # Get image.
             response = requests.get(image_url,
                                     proxies=self._current_proxies,
                                     headers=self.headers.get_headers())
             response.encode = "utf-8"
             response.raise_for_status()
 
-            # Move to separate function? Also, check if file already exists.
-            with open(os.path.join("Artists", file_name), "wb") as image_file:
+            # Move to separate function? Also, check if file already exists?
+            with open(file_name, "wb") as image_file:
                 for chunk in response.iter_content(100000):
                     image_file.write(chunk)
 
