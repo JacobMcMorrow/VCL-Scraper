a7e3f08e3b650f99728c4d19981dffe2b0d980d9
JacobMcMorrow <jacobmcmorrow@rogers.com>
2021-03-18 22:33:27 -0400

    Create initial logger module.

---
+++ logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""""""
+import os
+
+class Logger:
+    """"""
+
+    def __init__(self):
+        """"""
+        # Save line.
+
+    def log_error(self):
+        """"""
+        raise NotImplementedError
